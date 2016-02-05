afc0088d320842ef647e4846b0556df1779d28ad
Robert Mueller <Cookie42@users.noreply.github.com>
2016-02-05 15:13:34 +0100

    added input cache, saves commands like: r 90 f 20 needs to be added within motor

--- Source/PKeS/PKeS.ino
+++ Source/PKeS/PKeS.ino
@@ -9,11 +9,64 @@
 #include "Odometire.h"
 #include "PID.h"
 
+
+
+class InputCache
+{
+  static const int CacheSize=32;
+  char types[CacheSize];
+  int values[CacheSize];
+
+  int CSPointer=0;
+  int CEPointer=0;
+  int CSize=0;
+public:
+     
+  void AddValue(char type,int val)
+  {
+      if(CSize==CacheSize)
+      {
+      Serial.print("Cache full, the following Command Cant be added:");
+      Serial.print(type);
+      Serial.print(" ");
+      Serial.println(val);  
+      }
+      else
+      {
+       types[CEPointer]=type;
+       values[CEPointer]=val;
+       CEPointer=(CEPointer+1)%CacheSize;
+       CSize++; 
+      }
+  }
+
+  void GetValues(char& type,int& value)
+  {
+    if(CSPointer==CEPointer)
+    {
+      type='f';
+      value=0;
+    }
+    else
+    {
+    type=types[CSPointer];
+    value=values[CSPointer];
+    CSPointer=(CSPointer+1)%CacheSize;
+    CSize--;
+    }
+    
+  }
+};
+
+
+
+
 Display dis;
 MyGyro mygyro;
 Motor motor;
 Waterscale scale;
 ADConverter adc;
+InputCache ICache;
 
 int s1[10]={272,217,179,153,138,130,125,118,111,110};
 IRC irc1(s1);
@@ -70,7 +123,7 @@ void setup()
 
 
 static byte km=0;
-static int rot=0;
+static int val=0;
 static bool otherin=false;
 char t=0;
 
@@ -155,32 +208,46 @@ void loop()
                 // read the incoming byte:
                byte incomingByte = Serial.read();
                 //Serial.println(incomingByte);
-                static char lo=0;
+                static char type=0;
                 static int vorzeichen=1;
+                static bool samevalue=true;
                 if(otherin){
                   switch(incomingByte){
                     case '-':vorzeichen*=-1;break;
-                    case '1':rot=(rot*10) +1;break;
-                    case '2':rot=(rot*10) +2;break;
-                    case '3':rot=(rot*10) +3;break;
-                    case '4':rot=(rot*10) +4;break;
-                    case '5':rot=(rot*10) +5;break;
-                    case '6':rot=(rot*10) +6;break;
-                    case '7':rot=(rot*10) +7;break;
-                    case '8':rot=(rot*10) +8;break;
-                    case '9':rot=(rot*10) +9;break;
-                    case '0':rot=(rot*10) ;break;
+                    case '1':val=(val*10) +1;break;
+                    case '2':val=(val*10) +2;break;
+                    case '3':val=(val*10) +3;break;
+                    case '4':val=(val*10) +4;break;
+                    case '5':val=(val*10) +5;break;
+                    case '6':val=(val*10) +6;break;
+                    case '7':val=(val*10) +7;break;
+                    case '8':val=(val*10) +8;break;
+                    case '9':val=(val*10) +9;break;
+                    case '0':val=(val*10) ;break;
+                    
                     default:
-                          lo+=1;
-                          if(lo>3){
-                            rot=rot*12;
-                            mygyro.gyroChanged=vorzeichen*rot;
-                            vorzeichen=1;
-                            rot=0;
-                            otherin=false;
-                            t=5;
-                            lo=0;
-                          }
+
+                      if(samevalue)
+                      {
+                        samevalue=false;  
+                      }
+                      else
+                      {
+
+                        switch(type)
+                        {
+                          case 'f':ICache.addValue(type,val);break;
+                          case 'r':ICache.addValue(type,val*12);break;
+                          default:/*lol*/break;
+                        }
+                        vorzeichen=1;
+                        val=0;
+                        type=0;
+                        otherin=false;
+                        samevalue=true;
+                      }
+                          
+                          
                           break;
                   }
                 }else{
@@ -196,18 +263,22 @@ void loop()
                   case '7':t=7;break;
                   case '8':t=8;break;
                   case '9':t=9;break;
-                  case 'a':t=10;break;
-                  case 'b':t=11;break;
-                  case 'c':t=12;break;
-                  case 'd':t=13;break;
-                  case 'e':t=14;break;
-                  case 'f':t=15;break;
-                  case 'g':t=16;break;
-                  case 'h':t=17;break;
+                  //case 'a':t=10;break;
+                  //case 'b':t=11;break;
+                  //case 'c':t=12;break;
+                  //case 'd':t=13;break;
+                  //case 'e':t=14;break;
+                  //case 'f':t=15;break;
+                  //case 'g':t=16;break;
+                  //case 'h':t=17;break;
                   case 'w':km+=1;break;
                   case 'm':km-=1;break;
-                  case 'r':otherin=true;break;  
+                  case 'f':otherin=true;type='f';break;
+                  case 'r':otherin=true;type='r';break;  
                   default: break;
                 }  
                 }
 }
+
+
+
